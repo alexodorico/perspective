5bd9ba801f5e2e9f793d75d43e40e5729ef22848
alexodorico <alex.odorico@morleynet.com>
2019-07-01 20:04:04 -0400

    db connection

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const { Pool, Client } = require('pg');
+const pool = new Pool();
 const app = express();
 
 require('dotenv').config();
 
-app.get('/', (req, res) => {
-  console.log('request')
-});
+(async _ => {
+  pool.query('SELECT * FROM photo_post', (err, res) => {
+    console.log(err, res)
+    pool.end()
+  })
 
-app.listen(3000, _ => console.log('listenin'))
+  app.get('/', (req, res) => {
+    console.log('request')
+  });
+  
+  app.listen(3000, _ => console.log('listenin'))
+})();
